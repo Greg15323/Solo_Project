e1654162d9a97c0eba2c2d66e9d7784ef526e75d
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-03-08 12:49:34 -0700

    posts

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://greg15323:@cluster0.p0k3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://project:finalproject@cluster0.p0k3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
