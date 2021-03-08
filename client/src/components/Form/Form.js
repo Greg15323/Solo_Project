a6104307601e186209b4e46c6db098554aa780be
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-03-08 16:36:03 -0700

    added fucntion to create card

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
-
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
     
 const Form = () => {
@@ -10,19 +10,22 @@ const Form = () => {
     const handleSubmit = () => {
 
     }
+
+    const clear = () => {
+        
+    }
     
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
-            <Typography variant="h6">Creating a Post</Typography>
-            <TextField 
-                name="creator" 
-                variant="outlined" l
-                abel="Creator" 
-                fullWidth
-                value={postData.creator}
-                onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-            />
+                <Typography variant="h6">Creating a Post</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidthvalue={postData.creator}onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+                <TextField name="title" variant="outlined" label="Title" fullWidthvalue={postData.title}onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
+                <TextField name="message" variant="outlined" label="Message" fullWidthvalue={postData.message}onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
+                <TextField name="tags" variant="outlined" label="Tags" fullWidthvalue={postData.tags}onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
+                <div className={classes.fileInput}><FileBase type="file"multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}/></div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="samll" onClick={clear} fullWidth>Clear</Button>
             </form>
 
         </Paper>
