54d1f41bd6c049d7644470e47ce87bc9704038c2
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-03-08 17:58:08 -0700

    posts

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import * as api from '../api';
+import * as api from '../api/index.js';
 
 export const getPosts = () => async (dispatch) => {
     try {
@@ -7,5 +7,15 @@ export const getPosts = () => async (dispatch) => {
         dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
         console.log(error.message);
+    }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data });
+    } catch (error) {
+        console.log(error);
     }
 }
