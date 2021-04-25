3002592cbd6a7d525f34e1a6d7473bf6d1598283
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-04-25 16:54:12 -0600

    fixed some bugs

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextField, Grid, InputAdorment, IconButton } from '@material-ui/core';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
@@ -17,11 +17,11 @@ const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPas
                 type={type}
                 InputProps={name === 'password' && {
                     endAdornment: (
-                        <InputAdorment position ="end">
+                        <InputAdornment position ="end">
                             <IconButton onClick={handleShowPassword}>
                                 {type === "password" ? <Visibility /> : <VisibilityOff />}
                             </IconButton>
-                        </InputAdorment>
+                        </InputAdornment>
                     )
                 }}
             
