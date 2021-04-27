3ce202a18f8b6fab914aff125032b70a6d0110ea
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-04-27 12:28:25 -0600

    adding google button

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -46,6 +46,22 @@ const Auth = () => {
                             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
                             { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
                     </Grid>
+                    <GoogleLogin 
+                        clientId="GOOGLE ID"
+                        render={(renderProps) => (
+                            <Button 
+                            className={classes.googleButton} 
+                            color='primary' 
+                            fullWidth 
+                            onClick={renderProps.onClick} 
+                            disable={renderProps.disabled} 
+                            startIcon={<Icon />} 
+                            variant="contained"
+                            >
+                                Google Sign In
+                            </Button>
+                        )}
+                    />
                     <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                         {isSignup ? 'Sign Up' : 'Sign In' }
                     </Button>
