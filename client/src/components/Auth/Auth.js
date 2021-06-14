7dafda0444dcda3803a27249c529977175fb1106
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-06-13 19:58:48 -0600

    added initial state

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,11 +8,13 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles'; 
 import Input from './Input';
 
+const initialState = { firstName: '', lastName: '', email: '', passoword: '', confirmPassword: '' }; 
+
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
-    const [formData, setFormData] = useState();
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch ();
     const history = useHistory();
 
