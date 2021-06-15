b89fbb3e639e152b714b0819467aa504dd2a4d70
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-06-15 17:48:50 -0600

    fixed the mock up

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,22 +1,22 @@
 import { AUTH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
-export const signin = () => {FormData, history} async (dispatch) => {
+export const signin = (formData, history) => async (dispatch) => {
     try {
         // log in the user..
 
-        history.pushState('/');
+      history.push('/');
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
 };
 
-export const signup = () => {FormData, history} async (dispatch) => {
+export const signin = (formData, history) => async (dispatch) => {
     try {
         // sign up the user..
 
-        history.pushState('/');
+      history.push('/');
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
 };
