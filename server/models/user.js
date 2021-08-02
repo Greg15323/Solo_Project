4d785c46bee5c0d520820684cdad9c2d98ad2d2b
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-08-02 11:29:59 -0600

    adding userSchema

---
+++ server/models/user.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    name: { type: String, required: ture },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    id: { type: String }
+})
