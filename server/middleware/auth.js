f42a52903ddfb3b1364f1683d3b69e79b8da18af
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-08-20 00:00:10 -0600

    spacing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,7 +11,7 @@ const auth = async (req, res, next) => {
         let decodedData;
 
         if(token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'test');
+            decodedData = jwt.verify(token, 'test' );
 
             req.userId = decodedData?.id;
         } else {
