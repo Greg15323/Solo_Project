26ac130a467ae4304caa9b8495cc58436fe232bf
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-08-20 14:06:34 -0600

    header

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,8 +4,10 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
-        req.headers.Authorization
+        req.headers.Authorization = `Bearer ${JASON.parse(localStorage.getItem('profile')).token}`;
     }
+
+    return req;
 });
 
 export const fetchPosts = () => API.get('/posts');
